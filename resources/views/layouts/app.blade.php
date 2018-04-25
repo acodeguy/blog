90d3a22e8bfee4cdf637cdc5461014092a191d0a
acodeguy <24258342+acodeguy@users.noreply.github.com>
2018-04-25 22:10:52 +0100

    fixed mobile responsiveness

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -2,6 +2,7 @@
 <html>
   <head>
     <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.10/css/all.css" integrity="sha384-+d0P83n9kaQMCwj8F4RJB66tzIwOKmrdb46+porD/OvrJ+37WqIM7UoBtwHO6Nlg" crossorigin="anonymous">
     <link rel="stylesheet" href="/css/app.css">
     <link rel="stylesheet" href="/css/blog.css">
@@ -22,7 +23,7 @@
         <nav class="navbar navbar-expand-md navbar-light navbar-laravel">
             <div class="container">
                 <a class="navbar-brand" href="{{ url('/') }}">
-                    {{ config('app.name', 'The Blog') }}
+                    {{ config('app.name') }}
                 </a>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
@@ -86,6 +87,14 @@
       <div class="row">
 
         <div class="col-lg-12">
+
+          <div id="project-info">
+            <h1>{{ config('app.name') }}</h1>
+            <p>A blog site, running Laravel on by <a href="https://acodeguy.com">A Code Guy</a></p>
+            <a href="https://github.com/acodeguy/blog">
+              <button type="button" class="btn btn-info">View/Clone the Source Code on GitHub</button>
+            </a>
+          </div>
 
           @yield('content')
 
