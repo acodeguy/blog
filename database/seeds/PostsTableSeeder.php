ead4426fb04676e6a12bce94608bc44285f5c20a
acodeguy <24258342+acodeguy@users.noreply.github.com>
2018-06-23 16:06:56 +0100

    fixed hyperlink in seed file

--- database/seeds/PostsTableSeeder.php
+++ database/seeds/PostsTableSeeder.php
@@ -16,7 +16,7 @@ class PostsTableSeeder extends Seeder
             'id' => 1,
             'user_id' => 1,
             'title' => "View the Source Code on Github",
-            'body' => "Wanna see how this works or make a fork of your own? <a href='https://github.com/acodeguy/blog'>",
+            'body' => "Wanna see how this works or make a fork of your own? <a href='https://github.com/acodeguy/blog'>View the source on GitHub</a>.",
             'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
             'updated_at' => Carbon::now()->format('Y-m-d H:i:s'),
         ]);
