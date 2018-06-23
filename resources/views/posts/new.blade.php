ad295ab1c43473de713690ab26fd4e75a44ebf05
acodeguy <24258342+acodeguy@users.noreply.github.com>
2018-06-23 16:17:44 +0100

    delete newpost.js - added code to new post view

--- resources/views/posts/new.blade.php
+++ resources/views/posts/new.blade.php
@@ -25,7 +25,17 @@
 <script>
 $(document).ready(function(){
 
-$('#summernote').summernote();
+$('#summernote').summernote(
+  toolbar: [
+    // [groupName, [list of button]]
+    ['style', ['bold', 'italic', 'underline', 'clear']],
+    ['font', ['strikethrough', 'superscript', 'subscript']],
+    ['fontsize', ['fontsize']],
+    ['color', ['color']],
+    ['para', ['ul', 'ol', 'paragraph']],
+    ['height', ['height']]
+  ]
+);
 
 });
 </script>
