eb409eb966484d7d35c46f311a23aafb890623b6
acodeguy <24258342+acodeguy@users.noreply.github.com>
2018-06-23 16:05:36 +0100

    show html in posts in index view

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -10,7 +10,7 @@
         <div class="card index-post-card">
           <div class="card-body">
             <a href="/posts/{{ $post->id }}"><h3 class="card-title">{{ $post->title }}</h3></a>
-            <p class="card-text">{{ substr($post->body,0,255) }}. . .</p>
+            <p class="card-text">{!! substr($post->body,0,255) !!}. . .</p>
             <p>{{ $post->created_at->diffForHumans() }} by {{ $post->user->name }}</p>
             <a href="/posts/{{ $post->id }}">
               <button class="btn btn-primary" type="button" name="button">Read more...</button>
